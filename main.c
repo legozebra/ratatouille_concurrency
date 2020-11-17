2404a28632b7f59bf22455f971c19969c4372cf6
Kim Luong <kimnguyenluong@gmail.com>
2020-11-17 08:35:00 -0500

    Updated main.c with hints. Fixed service_customer() and get_help()

--- main.c
+++ main.c
@@ -1,28 +1,42 @@
 semaphore customers = 0;	// counts number of waiting customers
 semaphore chef = 0;			// number of chefs available (0 or 1)
 semaphore working = 0;		// can only service one customer at a time
-semaphore mutex = 1;		// for mutual exclusion of critical resource
+semaphore mutex = 1;		// for mutual exclusion of critical resource, used to prevent num_customers from being changed outside of the thread
+int max_customers = n;
 int num_customers = 0;		// number of customers 
 
 void chef() 
 {
-	// fill this out
+	while(1)
+	{
+		// --------------- FILL THIS OUT--------------
+		// Hints:
+		// There are three semaphores used here. Only one is locked and released in this loop.
+		// The chef should sleep (watch dramas) when there are no customers and then if there is a customer, start to service the customer. Keep in mind that when a customer is serviced, the number of customers waiting will have to be decremented.
+	}
 }
 void customer() 
 {
-	// fill this out
+	while(1)
+	{
+		// --------------- FILL THIS OUT--------------
+		// Hints:
+		// There are three semaphores used here. Only one is locked and released
+		// When a customer arrive, num_customers can change. However, num_customers will only change it it is less than max_customers. 
+	}
 }
 
-// Chef helps customer
+// Chef helps customer only if there is a customer to request it
+// Recall that working is initialized to 0. Chef can acquire the semaphore when working = 1 (customer gets help)
 service_customer()
 {
-	up(working);
+	down(working);
 }
 
-// Customer is helped
 get_help()
 {
-	down(working);
+	// Customer asks for help so now working = 1 (assuming working was 0 before)
+	up(working);
 }
 
 down(semaphore s)
@@ -33,7 +47,7 @@ down(semaphore s)
 	}
 	else
 	{
-		// sleep;
+		// sleep and watch all the dramas;
 	}
 }
 
